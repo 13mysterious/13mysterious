a39a26139688d9847e78dee8abf81213e1d38b5d
sumyeom <wjstndus75@naver.com>
2024-11-20 17:07:35 +0900

    feat: Add password edit feature
    1. 비밀번호 수정 기능 구현

--- src/main/java/com/example/newsfeed/controller/UserController.java
+++ src/main/java/com/example/newsfeed/controller/UserController.java
@@ -1,15 +1,13 @@
 package com.example.newsfeed.controller;
 
+import com.example.newsfeed.dto.UserPatchPasswordRequestDto;
 import com.example.newsfeed.dto.UserSignUpRequestDto;
 import com.example.newsfeed.dto.UserSignUpResponseDto;
 import com.example.newsfeed.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,5 +28,16 @@ public class UserController {
                         requestDto.getAge()
                 );
         return new ResponseEntity<>(userSignUpResponseDto, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{userId}/password")
+    public ResponseEntity<Void> updateUserPassword(
+            @PathVariable Long userId,
+            @RequestBody UserPatchPasswordRequestDto requestDto,
+            @SessionAttribute(name = "userId", required = false) Long sessionId
+    ){
+        userService.updateUserPassword(userId, requestDto.getOldPassword(), requestDto.getNewPassword(),sessionId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

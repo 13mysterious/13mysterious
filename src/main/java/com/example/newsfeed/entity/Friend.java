746ce84668c116d644a2e8fe73f5efc2eeab8b89
KM4HS <ac4hsu@gmail.com>
2024-11-20 13:48:31 +0900

    feat: Add friend entity

---
+++ src/main/java/com/example/newsfeed/entity/Friend.java
@@ -0,0 +1,34 @@
+package com.example.newsfeed.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+/**
+ * <ul>
+ * <li>packageName    : com.example.newsfeed.entity
+ * <li>fileName       : Friend
+ * <li>date           : 24. 11. 20.
+ * <li>description    : friend 테이블 엔티티
+ * </ul>
+ */
+
+@Getter
+@Entity
+@Table(name = "friend")
+public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private boolean isAccepted;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_user_id")
+    private User toUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_user_id")
+    private User fromUser;
+}

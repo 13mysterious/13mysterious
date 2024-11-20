3fe0edb95552be95b6e9e8d3f34095a05eef0093
suki <astre15@naver.com>
2024-11-20 21:01:01 +0900

    update: 게시물 목록 조회
    1.게시물 목록 조회

--- src/main/java/com/example/newsfeed/controller/BoardController.java
+++ src/main/java/com/example/newsfeed/controller/BoardController.java
@@ -5,10 +5,11 @@ import com.example.newsfeed.dto.BoardResponseDto;
 import com.example.newsfeed.dto.CreateBoardRequestDto;
 import com.example.newsfeed.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/boards")
@@ -17,19 +18,30 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    //게시물 저장
     @PostMapping
     public ResponseEntity<BoardResponseDto> save(
             @RequestBody CreateBoardRequestDto requestDto,
             @SessionAttribute(name ="userId") Long userId) {
 
-        //게시물 저장
         BoardResponseDto boardResponseDto =
                 boardService.save(
                         requestDto.getTitle(),
                         requestDto.getContents(),
                         userId
                 );
-
         return new ResponseEntity<>(boardResponseDto, HttpStatus.CREATED);
+    }
+
+    //게시물 목록 조회
+    @GetMapping("/{uerId}")
+    public ResponseEntity<List<BoardResponseDto>> findAllBoards(
+            @RequestParam Long userId,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "4") int size) {
+
+        List<BoardResponseDto> allBoardsDto = boardService.findAllBoards(userId, page, size);
+
+        return new ResponseEntity<>(allBoardsDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/newsfeed/service/BoardService.java
+++ src/main/java/com/example/newsfeed/service/BoardService.java
@@ -6,8 +6,14 @@ import com.example.newsfeed.entity.User;
 import com.example.newsfeed.repository.BoardRepository;
 import com.example.newsfeed.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +25,38 @@ public class BoardService {
     public BoardResponseDto save(String title, String contents, @SessionAttribute(name ="userId") Long userId) {
 
         User findUser = userRepository.findById(userId)
-                .orElseThrow(()-> new IllegalArgumentException("유저를 찾을 수 없습니다"));
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다"));
 
         Board board = new Board(findUser, title, contents, 0);
 
         //게시물 저장
         boardRepository.save(board);
 
-        return new BoardResponseDto(board.getId(),board.getTitle(), board.getContents());
+        return new BoardResponseDto(
+                board.getId(),
+                board.getTitle(),
+                board.getContents(),
+                findUser.getName(),
+                board.getLikeCount(),
+                board.getCreatedAt(),
+                board.getModifiedAt());
+    }
+    //게시물 목록 조회
+    public List<BoardResponseDto> findAllBoards(Long userId, int page, int size) {
+
+         Pageable pageable = PageRequest.of(page - 1, size);
+         Page<Board> boardPage = boardRepository.findAllByUserId(userId, pageable);
+
+         return boardPage.stream()
+                 .map(board -> new BoardResponseDto(
+                         board.getId(),
+                         board.getTitle(),
+                         board.getContents(),
+                         board.getUser().getName(), // User 객체에서 이름을 가져옵니다.
+                         board.getLikeCount(),
+                         board.getCreatedAt(),
+                         board.getModifiedAt()
+                 ))
+                 .collect(Collectors.toList());
     }
 }

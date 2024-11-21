8c3fd4f5ce34e0769965564c151f514788313d76
KM4HS <ac4hsu@gmail.com>
2024-11-21 21:33:33 +0900

    refactor: 세션키 상수 추가
    1. 세션키 상수 저장하는 Const 생성
    2. 세션키 사용하는 부분 모두 변환

---
+++ src/main/java/com/example/newsfeed/config/Const.java
@@ -0,0 +1,11 @@
+package com.example.newsfeed.config;
+
+/**
+ * <ul>
+ * <li>packageName    : com.example.newsfeed.config
+ * <li>fileName       : Const
+ * <li>date           : 24. 11. 21.
+ * <li>description    :
+ * </ul>
+ */public enum Const {
+}

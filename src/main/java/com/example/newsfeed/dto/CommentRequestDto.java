8845f7502ceb2d663f90b1ef35de87a02daa92a3
KM4HS <ac4hsu@gmail.com>
2024-11-22 14:42:32 +0900

    feat: 댓글 내용에 valid 조건 추가
    1. 댓글 내용은 빈칸일 수 없음
    2. 댓글은 100자 이내로 작성

--- src/main/java/com/example/newsfeed/dto/CommentRequestDto.java
+++ src/main/java/com/example/newsfeed/dto/CommentRequestDto.java
@@ -1,11 +1,15 @@
 package com.example.newsfeed.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 
 @Getter
 public class CommentRequestDto {
 
+    @NotBlank(message = "댓글 내용은 비울 수 없습니다.")
+    @Size(max = 100, message = "댓글은 100자 이내로 작성해야 합니다.")
     private String contents;
 
     public CommentRequestDto() {

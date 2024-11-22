119164e938cd690c69e1a1b4cc31055144c570d9
KM4HS <ac4hsu@gmail.com>
2024-11-22 19:34:42 +0900

    fix: 충돌 해결
    1. FriendRepository에서 잘못 머지된 메서드 수정

--- src/main/java/com/example/newsfeed/repository/FriendRepository.java
+++ src/main/java/com/example/newsfeed/repository/FriendRepository.java
@@ -12,7 +12,7 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     List<Friend> findByToUser(User user);
 
-    List<Long> findByFromUser_Id(Long UserId);
+    List<Friend> findByFromUser_IdAndIsAcceptedTrue(Long userId);
 
     Optional<Friend> findByToUserAndFromUser(User toUser, User fromUser);
 }
